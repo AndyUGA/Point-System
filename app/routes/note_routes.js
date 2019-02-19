7deaa4ad2339d267e2a03387c7928639f2c1d973
Andy <att35663@uga.edu>
2019-02-18 20:28:10 -0500

    Pulls data from mlab db

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -13,7 +13,7 @@ module.exports = function(app, db) {
 	//Displays home page
 	app.get('/', (req, res) => {
 
-		var collection = db.collection("notes");
+		var collection = db.collection("Members");
 		var currentNotes = [];
 
 		collection.find({}).toArray(function (err, result) {
@@ -23,14 +23,14 @@ module.exports = function(app, db) {
 			if(err) {
 				res.send({ 'error': ' An error has occurred'});
 			} else {
-				res.render('index', {currentNotes: result});
+				res.render('index', {result: result});
 			}
 		});
 	});
 
 	app.get('/LivePoints', (req, res) => {
 
-		var collection = db.collection("notes");
+		var collection = db.collection("Members");
 		var currentNotes = [];
 
 		collection.find({}).toArray(function (err, result) {
@@ -40,7 +40,7 @@ module.exports = function(app, db) {
 			if(err) {
 				res.send({ 'error': ' An error has occurred'});
 			} else {
-				res.render('LivePoints', {currentNotes: result});
+				res.render('LivePoints', {result: result});
 			}
 		});
 	});
--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: "mongodb://andy:test123@ds247688.mlab.com:47688/noteapp"
+		url: "mongodb://andy:test123@ds341825.mlab.com:41825/elementattendees"
 }
