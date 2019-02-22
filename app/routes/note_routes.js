374c00ce0d0723769d69f9f9b9cb48d86767e8c1
Andy <att35663@uga.edu>
2019-02-21 23:00:39 -0500

    Added ability to update house points

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -12,7 +12,7 @@ module.exports = function(app, db) {
 		var currentNotes = [];
 
 		collection.find({}).toArray(function (err, result) {
-			console.log(result);
+			//console.log(result);
 
 			if(err) {
 				res.send({ 'error': ' An error has occurred'});
@@ -28,7 +28,7 @@ module.exports = function(app, db) {
 		var currentNotes = [];
 
 		collection.find({}).toArray(function (err, result) {
-			console.log(result);
+			//console.log(result);
 
 			if(err) {
 				res.send({ 'error': ' An error has occurred'});
@@ -42,50 +42,124 @@ module.exports = function(app, db) {
 
 
 
-		var collection = db.collection("Members");
+
+
+
+
+
+		const collection = db.collection("Members");
 		const pointsToAdd = parseInt(req.params.points);
-
-
-		console.log("Point to add is " + pointsToAdd);
 		const name = req.params.name;
-
-
-		console.log("Name is " + name);
 		collection.find({}).toArray(function (err, result) {
 
 
-			//Loops through current attendees and updates score 
+			//Loops through current attendees
 			for(var i = 0; i < result.length; i++)
 			{
-				console.log("Comparing " + name + " with " + result[i].Name);
+
+
+				//Checks name from qrcode against database
 				if(name == result[i].Name)
 				{
+					//The current amount of points that the attendee has
+					const currentPoints = parseInt(result[i].Points);
 
-					const id = {'_id': new ObjectID(result[i]._id)};
-					console.log("id is " + id._id);
+					//The current id of the attendee
+					const attendeeID = {'_id': new ObjectID(result[i]._id)};
 
-					const currentPoint = parseInt(result[i].Points);
-					console.log("Current points from qr code is " + currentPoint);
+					//The current house of the attendee
+					const currentHouse = result[i].House;
 
-					const content = {Name: name, Points : currentPoint + pointsToAdd};
+					console.log("Current points: " + currentPoints);
+					console.log("Points to add : " + pointsToAdd);
 
-					db.collection('Members').update(id, content, (err, item) => {
+					const attendeeContent = { Name: name, House: currentHouse, Points : currentPoints + pointsToAdd};
+
+
+
+					//Update attendee points
+					db.collection('Members').update(attendeeID, attendeeContent, (err, item) => {
 						if(err) {
+							console.log("Error is " + err);
 							res.send({ 'Error is ': + err});
 						} else {
-								res.redirect('/LivePoints');
+							console.log('91');
+								res.redirect('/increaseHousePoints/' + currentHouse + '/' + pointsToAdd);
 						}
 					});
+
+
 					break;
 				}
 
 			}
+		});
 
 
 
 
+
+
+
+
+
+	})
+
+	app.get('/increaseHousePoints/:house/:pointsToAdd', (req, res) => {
+
+		//Update score for attendee house
+		const houseName = req.params.house
+		const pointsToAdd = req.params.pointsToAdd;
+		var currentHousePoints;
+		var houseID;
+
+		collection = db.collection("Houses");
+
+		collection.find({}).toArray(function (err, result) {
+			console.log('103');
+			console.log(result);
+
+
+
+			for(var i = 0; i < result.length; i++)
+			{
+				if(houseName == result[i].Name)
+				{
+					//Current id of house
+
+					houseID = {'_id': new ObjectID(result[i]._id)};
+
+					//Current points of the house
+					currentHousePoints = result[i].Points;
+
+
+					const totalHousePoints = parseInt(currentHousePoints) + parseInt(pointsToAdd);
+					var houseContent = {Name: result[i].Name, Points : totalHousePoints};
+
+					console.log('id:');
+					console.log(houseID);
+					console.log(houseContent);
+
+				db.collection('Houses').update(houseID, houseContent, (err, item) => {
+						if(err) {
+
+							console.log("Error is " + err);
+							res.send({ 'Error is ': + err});
+						} else {
+								console.log("House points updated");
+								res.redirect('/LivePoints');
+						}
+					});
+
+					break;
+				}
+
+			}
 		});
 	})
+
+
+
 
 	app.get('/defaultPoints', (req, res) => {
 
