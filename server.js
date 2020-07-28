e46a8cd21fb2b61651bb092f2e434b5153834dc5
Andy Truong <att35663@uga.edu>
2020-07-28 17:51:35 -0400

    Added css change

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
-const db = require("./config/db");
 const db2 = process.env.db;
 const methodOverride = require("method-override");
 
